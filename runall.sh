aefc8a6870a7049022fa7a600f85be82b44d48ee
qhlin <shu_rin@shu.edu.cn>
2021-01-01 20:29:14 +0800

    完成测试脚本

--- runall.sh
+++ runall.sh
@@ -1,8 +1,29 @@
-# cfg_name=(wsl_BasketballPass wsl_SlideEditing)
-cfg_name=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
+#!/bin/bash
+
+cfg_name=(BasketballPass SlideEditing)
+# cfg_name=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
+# cfg_name=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+
+test_1_frame=0
+if [ $test_1_frame -eq 1 ]; then
+    para="--FramesToBeEncoded=1"
+else
+    para=""
+fi
+
+basedir=`pwd`
 
 for i in "${!cfg_name[@]}";
 do
-    # ../bin/TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg --FramesToBeEncoded=1 > ${cfg_name[$i]}.log
-    ../bin/TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg  > ${cfg_name[$i]}.log
+    cd $basedir/bin_src
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
+     
+    # cd $basedir/bin_LBonly
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
+     
+    # cd $basedir/bin_LPonly
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
+     
+    # cd $basedir/bin_LBLP
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
 done
