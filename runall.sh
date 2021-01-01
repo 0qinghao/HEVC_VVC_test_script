5163423b89f232ffb040978dd1eb9e3a634c6004
qhlin <shu_rin@shu.edu.cn>
2021-01-01 21:10:14 +0800

    修改编解码脚本

--- runall.sh
+++ runall.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-cfg_name=(BasketballPass SlideEditing)
-# cfg_name=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
-# cfg_name=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+# cfg_name=(BasketballPass SlideEditing)
+# cfg_name_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
+cfg_name=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
 
-test_1_frame=0
+test_1_frame=1
 if [ $test_1_frame -eq 1 ]; then
     para="--FramesToBeEncoded=1"
 else
@@ -15,15 +15,26 @@ basedir=`pwd`
 
 for i in "${!cfg_name[@]}";
 do
+    echo "Encoding "${cfg_name[$i]}
     cd $basedir/bin_src
-    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
-     
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_src.log
     # cd $basedir/bin_LBonly
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
-     
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBonly.log
     # cd $basedir/bin_LPonly
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
-     
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LPonly.log
     # cd $basedir/bin_LBLP
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}.log
+    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBLP.log
+done
+
+for i in "${!cfg_name[@]}";
+do
+    echo "Decoding "${cfg_name[$i]}
+    cd $basedir/bin_src
+    ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_src.log
+    # cd $basedir/bin_LBonly
+    # ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LBonly.log
+    # cd $basedir/bin_LPonly
+    # ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LPonly.log
+    # cd $basedir/bin_LBLP
+    # ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LBLP.log
 done
