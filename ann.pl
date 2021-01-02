a69c6496f3ff0993c03033c7dee99bb11b9f5598
qhlin <shu_rin@shu.edu.cn>
2021-01-02 21:32:00 +0800

    完成分析脚本

---
+++ ann.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+# @cfg_list = ( PeopleOnStreet, Traffic,
+#     BasketballDrive,     BQTerrace,
+#     Cactus,              Kimono,
+#     ParkScene,           BasketballDrill,
+#     BQMall,              PartyScene,
+#     RaceHorsesC,         BasketballPass,
+#     BlowingBubbles,      BQSquare,
+#     RaceHorses,          FourPeople,
+#     Johnny,              KristenAndSara,
+#     BasketballDrillText, ChinaSpeed,
+#     SlideEditing,        SlideShow
+# );
+@cfg_list = ( BasketballPass, SlideEditing );
+
+# @dir_list = ( "./bin_src/", "./bin_LBonly/", "./bin_LPonly/", "./bin_LBLP/" );
+# @app_list = ( "_src.log",   "_LBonly.log",   "_LPonly.log",   "_LBLP.log" );
+@dir_list = ( "./bin_src/", "./bin_LBonly/" );
+@app_list = ( "_src.log",   "_LBonly.log" );
+
+# @dir_list = ("./bin_src/");
+# @app_list = ("_src.log");
+
+foreach $index ( 0 .. $#{dir_list} ) {
+    open( my $logfile, ">", $dir_list[$index] . $app_list[$index] . ".csv" );
+    print $logfile "Sequence, Bits, Encode_time, Decode_time\n";
+
+    foreach $cfg_name (@cfg_list) {
+        $filename = $dir_list[$index] . $cfg_name . "_enc" . $app_list[$index];
+        open( f, "<", $filename ) or die "未找到文件 $filename";
+        @lines = <f>;
+        foreach $line (@lines) {
+            if ( $line =~ /(?<=Bytes\ written\ to\ file:)\s*\d+/ ) {
+                $bits = $&;
+            }
+            elsif ( $line =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+                $enc_time = $&;
+                break;
+            }
+        }
+        close(f) or die "无法关闭文件 $filename";
+
+        $filename = $dir_list[$index] . $cfg_name . "_dec" . $app_list[$index];
+        open( f, "<", $filename ) or die "未找到文件 $filename";
+        @lines = <f>;
+        foreach $line (@lines) {
+            if ( $line =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+                $dec_time = $&;
+                break;
+            }
+        }
+        close(f) or die "无法关闭文件 $filename";
+
+        print $logfile $cfg_name;
+        print $logfile ",";
+        print $logfile $bits;
+        print $logfile ",";
+        print $logfile $enc_time;
+        print $logfile ",";
+        print $logfile $dec_time;
+        print $logfile "\n";
+    }
+
+    close(logfile);
+}
--- runall.sh
+++ runall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# cfg_name=(BasketballPass SlideEditing)
+cfg_name=(BasketballPass SlideEditing)
 # cfg_name_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
-cfg_name=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+# cfg_name=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
 
 test_1_frame=1
 if [ $test_1_frame -eq 1 ]; then
@@ -18,8 +18,8 @@ do
     echo "Encoding "${cfg_name[$i]}
     cd $basedir/bin_src
     ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_src.log
-    # cd $basedir/bin_LBonly
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBonly.log
+    cd $basedir/bin_LBonly
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBonly.log
     # cd $basedir/bin_LPonly
     # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LPonly.log
     # cd $basedir/bin_LBLP
@@ -31,8 +31,8 @@ do
     echo "Decoding "${cfg_name[$i]}
     cd $basedir/bin_src
     ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_src.log
-    # cd $basedir/bin_LBonly
-    # ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LBonly.log
+    cd $basedir/bin_LBonly
+    ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LBonly.log
     # cd $basedir/bin_LPonly
     # ./TAppDecoderStatic -b ./${cfg_name[$i]}.bin -o ${cfg_name[$i]}_dec.yuv > ./${cfg_name[$i]}_dec_LPonly.log
     # cd $basedir/bin_LBLP
