22f605884ff2d7c044e17540e261d6ca833f2ecc
qhlin <shu_rin@shu.edu.cn>
2021-01-16 19:44:12 +0800

    finishi

--- runall.sh
+++ runall.sh
@@ -8,8 +8,8 @@ BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartySc
 
 test_1_frame=1
 if [ $test_1_frame -eq 1 ]; then
-    para="--FramesToBeEncoded=10"
-    # para="--FramesToBeEncoded=1"
+    # para="--FramesToBeEncoded=10"
+    para="--FramesToBeEncoded=1"
     # para="--FramesToBeEncoded=200"
 else
     para=""
@@ -23,12 +23,14 @@ do
     echo "Encoding "${cfg_name[$i]}
     cd $basedir/bin_src
     ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_src.log
-    # cd $basedir/bin_LBonly
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBonly.log
-    # cd $basedir/bin_LPonly
-    # ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LPonly.log
+    cd $basedir/bin_LBonly
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBonly.log
+    cd $basedir/bin_LPonly
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LPonly.log
     cd $basedir/bin_LBLP
     ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_LBLP.log
+    cd $basedir/bin_src_forLBonly
+    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${cfg_name[$i]}.cfg $para > ./${cfg_name[$i]}_enc_src_forLBonly.log
 }&
 done
 
