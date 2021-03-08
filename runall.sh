f5901af9649bc70991091452380a51f076a89f6c
qhlin <shu_rin@shu.edu.cn>
2021-03-08 22:41:51 +0800

    坑：主配置文件中有一个降采样帧的选项 默认每8帧编码一帧

--- runall.sh
+++ runall.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# SeqName=(BasketballPass )
+SeqName=(BasketballPass )
 # SeqName_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
-SeqName=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+# SeqName=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
 # SeqName=(SlideEditing SlideShow FlyingGraphics)
 
 test_1_frame=1
 if [ $test_1_frame -eq 1 ]; then
-    # para="--FramesToBeEncoded=10"
-    para="--FramesToBeEncoded=1"
+    para="--FramesToBeEncoded=10"
+    # para="--FramesToBeEncoded=1"
     # para="--FramesToBeEncoded=100"
 else
     para=""
