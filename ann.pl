4f060816ebfd385825dcdc551faaf61190ff35d2
qhlin <shu_rin@shu.edu.cn>
2021-05-24 11:17:23 +0800

    无损测试完毕 速度比VTM提升10+倍 但是编码性能有的不如HEVC

--- ann.pl
+++ ann.pl
@@ -14,72 +14,88 @@
     # FlyingGraphics,
 );
 
-open( my $logfile, ">", "VVC_vs_HEVC.csv");
-print $logfile "Sequence, HEVC_Bits, HEVC_Time, VVC_Bits, VVC_Time, Ratio_Bits, x_Time\n";
-$avg_ratio_bits = 0;
-$avg_x_time = 0;
+open( my $logfile, ">", "hevc_vvc_vvenc.csv");
+# print $logfile "Sequence, HEVC_filesize, HEVC_Time, VVC_filesize, VVC_Time, Ratio_filesize, x_Time\n";
+print $logfile "Sequence, HEVC_filesize, HEVC_Time, VVC_filesize, VVC_Time, vvenc_filesize, vvenc_Time\n";
+# $avg_ratio_bits = 0;
+# $avg_x_time = 0;
 foreach $SeqName_name (@SeqName_list) {
     $filename_HEVC = "./bin_HEVC/" . $SeqName_name . "_enc" . "_HEVC.log";
     $filename_VVC = "./bin_VVC/" . $SeqName_name . "_enc" . "_VVC.log";
+    $filename_vvenc = "./bin_vvenc/" . $SeqName_name . "_enc" . "_vvenc.log";
+    $encfilename_HEVC = "./bin_HEVC/" . $SeqName_name . ".bin";
+    $encfilename_VVC = "./bin_VVC/" . $SeqName_name . ".bin";
+    $encfilename_vvenc = "./bin_vvenc/" . $SeqName_name . ".bin";
 
     open( f_HEVC, "<", $filename_HEVC ) or die "未找到文件 $filename_HEVC";
     @lines_HEVC = <f_HEVC>;
     open( f_VVC, "<", $filename_VVC ) or die "未找到文件 $filename_VVC";
     @lines_VVC = <f_VVC>;
+    open( f_vvenc, "<", $filename_vvenc ) or die "未找到文件 $filename_vvenc";
+    @lines_vvenc = <f_vvenc>;
 
     foreach $line_HEVC (@lines_HEVC) {
-        if ( $line_HEVC =~ /(?<=\ 10\ {4}a)\s*[\d\.]+/ ) {
-            $Bits_HEVC = $&;
-        }
-        elsif ( $line_HEVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+        if ( $line_HEVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
             $enc_time_HEVC = $&;
             break;
         }
     }
+    $filesize_HEVC = (stat($encfilename_HEVC))[7];
     close(f_HEVC) or die "无法关闭文件 $filename_HEVC";
 
     foreach $line_VVC (@lines_VVC) {
-        if ( $line_VVC =~ /(?<=\ 10\ {4}a)\s*[\d\.]+/ ) {
-            $Bits_VVC = $&;
-        }
-        elsif ( $line_VVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+        if ( $line_VVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
             $enc_time_VVC = $&;
             break;
         }
     }
+    $filesize_VVC = (stat($encfilename_VVC))[7];
     close(f_VVC) or die "无法关闭文件 $filename_VVC";
+
+    foreach $line_vvenc (@lines_vvenc) {
+        if ( $line_vvenc =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+            $enc_time_vvenc = $&;
+            break;
+        }
+    }
+    $filesize_vvenc = (stat($encfilename_vvenc))[7];
+    close(f_vvenc) or die "无法关闭文件 $filename_vvenc";
 
     print $logfile $SeqName_name;
     print $logfile ",";
-    print $logfile $Bits_HEVC;
+    print $logfile $filesize_HEVC;
     print $logfile ",";
     print $logfile $enc_time_HEVC;
     print $logfile ",";
-    print $logfile $Bits_VVC;
+    print $logfile $filesize_VVC;
     print $logfile ",";
     print $logfile $enc_time_VVC;
     print $logfile ",";
-    print $logfile ($Bits_VVC - $Bits_HEVC)/$Bits_HEVC*100;
+    print $logfile $filesize_vvenc;
     print $logfile ",";
-    print $logfile $enc_time_VVC/$enc_time_HEVC;
+    print $logfile $enc_time_vvenc;
     print $logfile ",";
+    # print $logfile ($filesize_VVC - $filesize_HEVC)/$filesize_HEVC*100;
+    # print $logfile ",";
+    # print $logfile $enc_time_VVC/$enc_time_HEVC;
+    # print $logfile ",";
     print $logfile "\n";
 
-    $avg_ratio_bits = $avg_ratio_bits + ($Bits_VVC - $Bits_HEVC)/$Bits_HEVC*100/(1+$#{SeqName_list});
-    $avg_x_time = $avg_x_time + $enc_time_VVC/$enc_time_HEVC/(1+$#{SeqName_list});
+    # $avg_ratio_bits = $avg_ratio_bits + ($filesize_VVC - $filesize_HEVC)/$filesize_HEVC*100/(1+$#{SeqName_list});
+    # $avg_x_time = $avg_x_time + $enc_time_VVC/$enc_time_HEVC/(1+$#{SeqName_list});
 }
-print $logfile "AVG";
-print $logfile ",";
-print $logfile "-";
-print $logfile ",";
-print $logfile "-";
-print $logfile ",";
-print $logfile "-";
-print $logfile ",";
-print $logfile "-";
-print $logfile ",";
-print $logfile $avg_ratio_bits;
-print $logfile ",";
-print $logfile $avg_x_time;
-print $logfile ",";
+# print $logfile "AVG";
+# print $logfile ",";
+# print $logfile "-";
+# print $logfile ",";
+# print $logfile "-";
+# print $logfile ",";
+# print $logfile "-";
+# print $logfile ",";
+# print $logfile "-";
+# print $logfile ",";
+# print $logfile $avg_ratio_bits;
+# print $logfile ",";
+# print $logfile $avg_x_time;
+# print $logfile ",";
 close(logfile);
--- runall.sh
+++ runall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SeqName=(BasketballPass )
+# SeqName=(BasketballPass)
 # SeqName_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
 SeqName=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
 # SeqName=(SlideEditing SlideShow FlyingGraphics)
@@ -14,15 +14,17 @@ else
     para=""
 fi
 
-basedir=`pwd`
+basedir=$(pwd)
 
-for i in "${!SeqName[@]}";
-do
-{
-    echo "Encoding "${SeqName[$i]}
-    cd $basedir/bin_HEVC
-    ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para > ./${SeqName[$i]}_enc_HEVC.log 
-    cd $basedir/bin_VVC
-    ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para > ./${SeqName[$i]}_enc_VVC.log 
-}
+for i in "${!SeqName[@]}"; do
+    {
+        echo "Encoding "${SeqName[$i]}
+        cd $basedir/bin_HEVC
+        ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_HEVC.log
+        cd $basedir/bin_VVC
+        ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC.log
+        cd $basedir/bin_vvenc
+        # ./vvencFFapp -c ../cfg_vvenc/intra_lossless.cfg -c ../cfg_vvenc/vtm/encoder_intra_vtm.cfg -c ../cfg_vvenc/vtm/lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_vvenc.log
+        ./vvencFFapp -c ../cfg_vvenc/intra_lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_vvenc.log
+    }
 done
