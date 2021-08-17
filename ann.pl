643d7889b8d50294ada71348b22c6876ffd0e06c
qhlin <qhlin96@foxmail.com>
2021-08-17 20:01:32 +0800

    LIP优化测试完毕

--- ann.pl
+++ ann.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl
 @SeqName_list = (
+    Tango2,              FoodMarket4,
     PeopleOnStreet,      Traffic,
     BasketballDrive,     BQTerrace,
     Cactus,              Kimono,
@@ -11,91 +12,78 @@
     Johnny,              KristenAndSara,
     BasketballDrillText, ChinaSpeed,
     SlideEditing,        SlideShow,
-    # FlyingGraphics,
 );
+# @SeqName_list = (
+#     BasketballPass,
+#     BlowingBubbles,      BQSquare,
+#     SlideEditing,        SlideShow,
+# );
 
-open( my $logfile, ">", "hevc_vvc_vvenc.csv");
-# print $logfile "Sequence, HEVC_filesize, HEVC_Time, VVC_filesize, VVC_Time, Ratio_filesize, x_Time\n";
-print $logfile "Sequence, HEVC_filesize, HEVC_Time, VVC_filesize, VVC_Time, vvenc_filesize, vvenc_Time\n";
-# $avg_ratio_bits = 0;
-# $avg_x_time = 0;
+open( my $logfile, ">", "vtm_src_lip.csv");
+print $logfile "Sequence, src_filesize, src_Time, lip_filesize, lip_Time, Ratio_filesize, x_Time\n";
+$avg_ratio_bits = 0;
+$avg_x_time = 0;
 foreach $SeqName_name (@SeqName_list) {
-    $filename_HEVC = "./bin_HEVC/" . $SeqName_name . "_enc" . "_HEVC.log";
-    $filename_VVC = "./bin_VVC/" . $SeqName_name . "_enc" . "_VVC.log";
-    $filename_vvenc = "./bin_vvenc/" . $SeqName_name . "_enc" . "_vvenc.log";
-    $encfilename_HEVC = "./bin_HEVC/" . $SeqName_name . ".bin";
-    $encfilename_VVC = "./bin_VVC/" . $SeqName_name . ".bin";
-    $encfilename_vvenc = "./bin_vvenc/" . $SeqName_name . ".bin";
+    print $SeqName_name;
+    $filename_VVC_lip = "./bin_VVC_lip/" . $SeqName_name . "_enc" . "_VVC_lip.log";
+    $filename_VVC_src = "./bin_VVC_src/" . $SeqName_name . "_enc" . "_VVC_src.log";
+    $encfilename_VVC_lip = "./bin_VVC_lip/" . $SeqName_name . ".bin";
+    $encfilename_VVC_src = "./bin_VVC_src/" . $SeqName_name . ".bin";
 
-    open( f_HEVC, "<", $filename_HEVC ) or die "未找到文件 $filename_HEVC";
-    @lines_HEVC = <f_HEVC>;
-    open( f_VVC, "<", $filename_VVC ) or die "未找到文件 $filename_VVC";
-    @lines_VVC = <f_VVC>;
-    open( f_vvenc, "<", $filename_vvenc ) or die "未找到文件 $filename_vvenc";
-    @lines_vvenc = <f_vvenc>;
+    open( f_VVC_lip, "<", $filename_VVC_lip ) or die "未找到文件 $filename_VVC_lip";
+    @lines_VVC_lip = <f_VVC_lip>;
+    open( f_VVC_src, "<", $filename_VVC_src ) or die "未找到文件 $filename_VVC_src";
+    @lines_VVC_src = <f_VVC_src>;
 
-    foreach $line_HEVC (@lines_HEVC) {
-        if ( $line_HEVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
-            $enc_time_HEVC = $&;
+    foreach $line_VVC_lip (@lines_VVC_lip) {
+        if ( $line_VVC_lip =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+            $enc_time_VVC_lip = $&;
             break;
         }
     }
-    $filesize_HEVC = (stat($encfilename_HEVC))[7];
-    close(f_HEVC) or die "无法关闭文件 $filename_HEVC";
+    $filesize_VVC_lip = (stat($encfilename_VVC_lip))[7];
+    close(f_VVC_lip) or die "无法关闭文件 $filename_VVC_lip";
 
-    foreach $line_VVC (@lines_VVC) {
-        if ( $line_VVC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
-            $enc_time_VVC = $&;
+    foreach $line_VVC_src (@lines_VVC_src) {
+        if ( $line_VVC_src =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+            $enc_time_VVC_src = $&;
             break;
         }
     }
-    $filesize_VVC = (stat($encfilename_VVC))[7];
-    close(f_VVC) or die "无法关闭文件 $filename_VVC";
-
-    foreach $line_vvenc (@lines_vvenc) {
-        if ( $line_vvenc =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
-            $enc_time_vvenc = $&;
-            break;
-        }
-    }
-    $filesize_vvenc = (stat($encfilename_vvenc))[7];
-    close(f_vvenc) or die "无法关闭文件 $filename_vvenc";
+    $filesize_VVC_src = (stat($encfilename_VVC_src))[7];
+    close(f_VVC_src) or die "无法关闭文件 $filename_VVC_src";
 
     print $logfile $SeqName_name;
     print $logfile ",";
-    print $logfile $filesize_HEVC;
+    print $logfile $filesize_VVC_src;
     print $logfile ",";
-    print $logfile $enc_time_HEVC;
+    print $logfile $enc_time_VVC_src;
     print $logfile ",";
-    print $logfile $filesize_VVC;
+    print $logfile $filesize_VVC_lip;
     print $logfile ",";
-    print $logfile $enc_time_VVC;
+    print $logfile $enc_time_VVC_lip;
     print $logfile ",";
-    print $logfile $filesize_vvenc;
+    print $logfile ($filesize_VVC_lip - $filesize_VVC_src)/$filesize_VVC_src*100;
     print $logfile ",";
-    print $logfile $enc_time_vvenc;
+    print $logfile $enc_time_VVC_lip/$enc_time_VVC_src;
     print $logfile ",";
-    # print $logfile ($filesize_VVC - $filesize_HEVC)/$filesize_HEVC*100;
-    # print $logfile ",";
-    # print $logfile $enc_time_VVC/$enc_time_HEVC;
-    # print $logfile ",";
     print $logfile "\n";
 
-    # $avg_ratio_bits = $avg_ratio_bits + ($filesize_VVC - $filesize_HEVC)/$filesize_HEVC*100/(1+$#{SeqName_list});
-    # $avg_x_time = $avg_x_time + $enc_time_VVC/$enc_time_HEVC/(1+$#{SeqName_list});
+    $avg_ratio_bits = $avg_ratio_bits + ($filesize_VVC_lip - $filesize_VVC_src)/$filesize_VVC_src*100/(1+$#{SeqName_list});
+    $avg_x_time = $avg_x_time + $enc_time_VVC_lip/$enc_time_VVC_src/(1+$#{SeqName_list});
 }
-# print $logfile "AVG";
-# print $logfile ",";
-# print $logfile "-";
-# print $logfile ",";
-# print $logfile "-";
-# print $logfile ",";
-# print $logfile "-";
-# print $logfile ",";
-# print $logfile "-";
-# print $logfile ",";
-# print $logfile $avg_ratio_bits;
-# print $logfile ",";
-# print $logfile $avg_x_time;
-# print $logfile ",";
+print $logfile "AVG";
+print $logfile ",";
+print $logfile "-";
+print $logfile ",";
+print $logfile "-";
+print $logfile ",";
+print $logfile "-";
+print $logfile ",";
+print $logfile "-";
+print $logfile ",";
+print $logfile $avg_ratio_bits;
+print $logfile ",";
+print $logfile $avg_x_time;
+print $logfile ",";
 close(logfile);
--- runall.sh
+++ runall.sh
@@ -2,13 +2,16 @@
 
 # SeqName=(BasketballPass)
 # SeqName_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
-SeqName=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+# SeqName=(Tango2 FoodMarket4 PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
+SeqName=(Tango2 FoodMarket4)
+# SeqName=(BasketballPass BlowingBubbles BQSquare SlideEditing SlideShow)
 # SeqName=(SlideEditing SlideShow FlyingGraphics)
 
 test_1_frame=1
+test_time=0
 if [ $test_1_frame -eq 1 ]; then
-    para="--FramesToBeEncoded=10"
-    # para="--FramesToBeEncoded=1"
+    # para="--FramesToBeEncoded=10"
+    para="--FramesToBeEncoded=1"
     # para="--FramesToBeEncoded=100"
 else
     para=""
@@ -16,15 +19,24 @@ fi
 
 basedir=$(pwd)
 
-for i in "${!SeqName[@]}"; do
-    {
-        echo "Encoding "${SeqName[$i]}
-        cd $basedir/bin_HEVC
-        ./TAppEncoderStatic -c ../cfg/encoder_intra_main_LL.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_HEVC.log
-        cd $basedir/bin_VVC
-        ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC.log
-        cd $basedir/bin_vvenc
-        # ./vvencFFapp -c ../cfg_vvenc/intra_lossless.cfg -c ../cfg_vvenc/vtm/encoder_intra_vtm.cfg -c ../cfg_vvenc/vtm/lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_vvenc.log
-        ./vvencFFapp -c ../cfg_vvenc/intra_lossless.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_vvenc.log
-    }
-done
+if [ $test_time -eq 1 ]; then
+    for i in "${!SeqName[@]}"; do
+        {
+            echo "Encoding "${SeqName[$i]}
+            cd $basedir/bin_VVC_src
+            ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg_VVC/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC_src.log
+            cd $basedir/bin_VVC_lip
+            ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg_VVC/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC_lip.log
+        }
+    done
+else
+    for i in "${!SeqName[@]}"; do
+        {
+            echo "Encoding "${SeqName[$i]}
+            cd $basedir/bin_VVC_src
+            ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg_VVC/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC_src.log &
+            cd $basedir/bin_VVC_lip
+            ./EncoderAppStatic -c ../cfg_VVC/encoder_intra_vtm.cfg -c ../cfg_VVC/lossless/lossless.cfg -c ../cfg_VVC/per-sequence/${SeqName[$i]}.cfg $para >./${SeqName[$i]}_enc_VVC_lip.log &
+        } &
+    done
+fi
