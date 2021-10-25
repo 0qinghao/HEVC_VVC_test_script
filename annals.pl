b61d92f28826e329e279155ac539a94c0c441c34
qhlin <qhlin96@foxmail.com>
2021-10-25 20:22:05 +0800

    TS NOTS test

--- annals.pl
+++ annals.pl
@@ -1,32 +1,17 @@
 #!/usr/bin/perl
-@SeqName_list = (
-    # PeopleOnStreet,      Traffic,
-
-    # BasketballDrive,     BQTerrace,
-    # Cactus,              Kimono,
-    # ParkScene,           
-
-    BasketballDrill,     BQMall,
-    PartyScene,          RaceHorsesC,
-
-    BasketballPass,      BlowingBubbles,      
-    BQSquare,            RaceHorses,
-    
-    FourPeople,          Johnny,
-    KristenAndSara,
-
-    BasketballDrillText, ChinaSpeed,
-    SlideEditing,        SlideShow,
+@SeqName_list=(
+    "10_outdoor_dusk_cross_lightvar_1920x1088", "7_indoor_night_noPeople_1920x1088", "outdoor_day_8M_1920x1088_872frames", "12_indoor_day_OnePeopleSitting_1920x1088", "8_outdoor_day_road_many_cars_1920x1088", "outdoor_day_cross_clip_global_light_change2_1920x1088_1000frames", "13_outdoor_day_road_pixelmove_1920x1088", "9_indoor_day_canteen_1920x1088", "outdoor_night_8M_1920x1088", "1_outdoor_day_cross_1920x1088", "hk_floor_12M_1920x1088", "outdoor_wdroff_8M_1920x1088", "2_outdoor_dusk_cross_1920x1088", "indoor1_54db_wdroff_NR50_8M_1920x1088", "question_IFlicker_1_1920x1088_673frames", "3_outdoor_night_cross_1920x1088", "indoor_54db_wdroff_NR35_8fps_8M_1920x1088_1399frames", "question_IFlicker_2_1920x1088_1400frames", "4_indoor_night_onePeopleMoving_1920x1088", "indoor_54db_wdroff_NR40_8fps_8M_1920x1088_1397frames", "red_car2_1920x1088", "5_outdoor_day_rain_cross_1920x1088", "indoor_54db_wdroff_NR45_8fps_8M_1920x1088", "6_outdoor_day_road_fewcars_1920x1088", "outdoor_day02_1920x1088"
 );
-@QP_list = (22, 27, 32, 37);
+# outdoor_day_8M_1920x1088_872frames,hk_floor_12M_1920x1088,question_IFlicker_1_1920x1088_673frames,question_IFlicker_2_1920x1088_1400frames,indoor_54db_wdroff_NR45_8fps_8M_1920x1088
+@QP_list = (37, 41, 45, 49);
 
-open( my $logfile, ">", "hevc_vvc_vvenc.csv");
+open( my $logfile, ">", "TS_noTS.csv");
 
-print $logfile "HM Part\n";
+print $logfile "TS Part\n";
 print $logfile "Sequence, QP, kbps, Ypsnr, Upsnr, Vpsnr, EncTime\n";
 foreach $SeqName_name (@SeqName_list) {
     foreach $QP (@QP_list) {
-        $logfilename_HM = "./bin_HEVC/" . $SeqName_name . "_qp" . $QP . "_enc" . "_HEVC.log";
+        $logfilename_HM = "./bin_HEVC_src/" . $SeqName_name . "_qp" . $QP . "_enc" . "_TS.log";
         open( f_HM, "<", $logfilename_HM ) or die "未找到文件 $logfilename_HM";
         @lines_HM = <f_HM>;
         foreach $line_HM (@lines_HM) {
@@ -64,85 +49,45 @@ foreach $SeqName_name (@SeqName_list) {
     }
 }
 
-print $logfile "VTM Part\n";
+print $logfile "noTS Part\n";
 print $logfile "Sequence, QP, kbps, Ypsnr, Upsnr, Vpsnr, EncTime\n";
 foreach $SeqName_name (@SeqName_list) {
     foreach $QP (@QP_list) {
-        $logfilename_VTM = "./bin_VVC/" . $SeqName_name . "_qp" . $QP . "_enc" . "_VVC.log";
-        open( f_VTM, "<", $logfilename_VTM ) or die "未找到文件 $logfilename_VTM";
-        @lines_VTM = <f_VTM>;
-        foreach $line_VTM (@lines_VTM) {
+        $logfilename_VTM = "./bin_HEVC_noTS/" . $SeqName_name . "_qp" . $QP . "_enc" . "_noTS.log";
+        open( f_HM, "<", $logfilename_VTM ) or die "未找到文件 $logfilename_VTM";
+        @lines_HM = <f_HM>;
+        foreach $line_HM (@lines_HM) {
             if ($next_2_line_log == 1)
             {
                 $next_1_line_log = 1;
                 $next_2_line_log = 0;
             }
-            if ($line_VTM =~ /LayerId\s{2}0/ ) {
+            if ($line_HM =~ /SUMMARY/ ) {
                 $next_2_line_log = 1;
             }
             if ($next_1_line_log == 1) {
-                if ($line_VTM =~ /([\d\.]+\s+){4}/ ) {
-                    $VTM_kbpsYUVpsnr = $&;
-                    $VTM_kbpsYUVpsnr =~ s/\s+/,/g;
-                    # print $VTM_kbpsYUVpsnr;
+                if ($line_HM =~ /([\d\.]+\s+){4}/ ) {
+                    $HM_kbpsYUVpsnr = $&;
+                    $HM_kbpsYUVpsnr =~ s/\s+/,/g;
+                    # print $HM_kbpsYUVpsnr;
                     $next_1_line_log = 0;
                 }
             }
-            elsif ( $line_VTM =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
-                $VTM_EncTime = $&;
-                $VTM_EncTime =~ s/\s+//g;
-                # print $VTM_EncTime;
+            elsif ( $line_HM =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
+                $HM_EncTime = $&;
+                $HM_EncTime =~ s/\s+//g;
+                # print $HM_EncTime;
                 break;
             }
         }
-        close(f_VTM) or die "无法关闭文件 $logfilename_VTM";
+        close(f_HM) or die "无法关闭文件 $logfilename_HM";
         print $logfile $SeqName_name;
         print $logfile ",";
         print $logfile $QP;
         print $logfile ",";
-        print $logfile $VTM_kbpsYUVpsnr;
-        print $logfile $VTM_EncTime;
+        print $logfile $HM_kbpsYUVpsnr;
+        print $logfile $HM_EncTime;
         print $logfile "\n";
-    }
-}
-
-@vvenc_level_list = (slow, medium, fast, faster);
-foreach $vvenc_level (@vvenc_level_list) {
-    print $logfile "VVenC " . $vvenc_level . " Part\n";
-    print $logfile "Sequence, QP, kbps, Ypsnr, Upsnr, Vpsnr, EncTime\n";
-    foreach $SeqName_name (@SeqName_list) {
-        foreach $QP (@QP_list) {
-            $logfilename_VVenC = "./bin_vvenc/" . $SeqName_name . "_qp" . $QP . "_" . $vvenc_level . "_enc" . "_vvenc.log";
-            open( f_VVenC, "<", $logfilename_VVenC ) or die "未找到文件 $logfilename_VVenC";
-            @lines_VVenC = <f_VVenC>;
-            foreach $line_VVenC (@lines_VVenC) {
-                if ($line_VVenC =~ /Total Frames/ ) {
-                    $next_1_line_log = 1;
-                }
-                if ($next_1_line_log == 1) {
-                    if ($line_VVenC =~ /([\d\.]+\s+){4}/ ) {
-                        $VVenC_kbpsYUVpsnr = $&;
-                        $VVenC_kbpsYUVpsnr =~ s/\s+/,/g;
-                        # print $VVenC_kbpsYUVpsnr;
-                        $next_1_line_log = 0;
-                    }
-                }
-                elsif ( $line_VVenC =~ /(?<=\ Total\ Time:)\s*[\d\.]+/ ) {
-                    $VVenC_EncTime = $&;
-                    $VVenC_EncTime =~ s/\s+//g;
-                    # print $VVenC_EncTime;
-                    break;
-                }
-            }
-            close(f_VVenC) or die "无法关闭文件 $logfilename_VVenC";
-            print $logfile $SeqName_name;
-            print $logfile ",";
-            print $logfile $QP;
-            print $logfile ",";
-            print $logfile $VVenC_kbpsYUVpsnr;
-            print $logfile $VVenC_EncTime;
-            print $logfile "\n";
-        }
     }
 }
 
--- runall.sh
+++ runall.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-# SeqName=(BasketballPass)
-# SeqName_alphabet_all=(BasketballDrill BlowingBubbles Cactus Kimono PartyScene SlideEditing Vidyo1 BasketballDrillText BQMall ChinaSpeed KristenAndSara PeopleOnStreet SlideShow Vidyo3 BasketballDrive BQSquare FourPeople RaceHorsesC Vidyo4 BasketballPass BQTerrace Johnny ParkScene RaceHorses Traffic)
-# SeqName=(PeopleOnStreet Traffic BasketballDrive BQTerrace Cactus Kimono ParkScene BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
-SeqName=(BasketballDrill BQMall PartyScene RaceHorsesC BasketballPass BlowingBubbles BQSquare RaceHorses FourPeople Johnny KristenAndSara BasketballDrillText ChinaSpeed SlideEditing SlideShow)
-# SeqName=(SlideEditing SlideShow FlyingGraphics)
-qplist=(22 27 32 37)
+SeqName=(10_outdoor_dusk_cross_lightvar_1920x1088 7_indoor_night_noPeople_1920x1088 outdoor_day_8M_1920x1088_872frames 12_indoor_day_OnePeopleSitting_1920x1088 8_outdoor_day_road_many_cars_1920x1088 outdoor_day_cross_clip_global_light_change2_1920x1088_1000frames 13_outdoor_day_road_pixelmove_1920x1088 9_indoor_day_canteen_1920x1088 outdoor_night_8M_1920x1088 1_outdoor_day_cross_1920x1088 hk_floor_12M_1920x1088 outdoor_wdroff_8M_1920x1088 2_outdoor_dusk_cross_1920x1088 indoor1_54db_wdroff_NR50_8M_1920x1088 question_IFlicker_1_1920x1088_673frames 3_outdoor_night_cross_1920x1088 indoor_54db_wdroff_NR35_8fps_8M_1920x1088_1399frames question_IFlicker_2_1920x1088_1400frames 4_indoor_night_onePeopleMoving_1920x1088 indoor_54db_wdroff_NR40_8fps_8M_1920x1088_1397frames red_car2_1920x1088 5_outdoor_day_rain_cross_1920x1088 indoor_54db_wdroff_NR45_8fps_8M_1920x1088 6_outdoor_day_road_fewcars_1920x1088 outdoor_day02_1920x1088)
+# SeqName=(outdoor_day_8M_1920x1088_872frames hk_floor_12M_1920x1088 question_IFlicker_1_1920x1088_673frames question_IFlicker_2_1920x1088_1400frames indoor_54db_wdroff_NR45_8fps_8M_1920x1088)
+qplist=(37 41 45 49)
 
-test_1_frame=1
+test_1_frame=0
 if [ $test_1_frame -eq 1 ]; then
-    # para="--FramesToBeEncoded=1"
-    para="--FramesToBeEncoded=32"
-    # para="--FramesToBeEncoded=1"
-    # para="--FramesToBeEncoded=100"
+    para="--FramesToBeEncoded=1"
 else
     para=""
 fi
@@ -22,23 +16,12 @@ basedir=$(pwd)
 for i in "${!SeqName[@]}"; do
     for qpind in "${!qplist[@]}"; do
         {
-            cd $basedir/bin_HEVC
-            echo "Encoding "${SeqName[$i]}" HM"
-            ./TAppEncoderStatic -c ../cfg/encoder_randomaccess_main.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_enc_HEVC.log
+            echo "Encoding "${SeqName[$i]}
 
-            cd $basedir/bin_VVC
-            echo "Encoding "${SeqName[$i]}" VTM"
-            ./EncoderAppStatic -c ../cfg_VVC/encoder_randomaccess_vtm.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_enc_VVC.log
-
-            cd $basedir/bin_vvenc
-            echo "Encoding "${SeqName[$i]}" VVenC slow"
-            ./vvencFFapp -c ../cfg_vvenc/randomaccess_slow.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_slow_enc_vvenc.log
-            echo "Encoding "${SeqName[$i]}" VVenC medium"
-            ./vvencFFapp -c ../cfg_vvenc/randomaccess_medium.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_medium_enc_vvenc.log
-            echo "Encoding "${SeqName[$i]}" VVenC fast"
-            ./vvencFFapp -c ../cfg_vvenc/randomaccess_fast.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_fast_enc_vvenc.log
-            echo "Encoding "${SeqName[$i]}" VVenC faster"
-            ./vvencFFapp -c ../cfg_vvenc/randomaccess_faster.cfg -c ../cfg/per-sequence/${SeqName[$i]}.cfg $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_faster_enc_vvenc.log
+            cd $basedir/bin_HEVC_noTS
+            ./TAppEncoderStatic -c ../cfg_FH420/encoder_intra_main_MOL.cfg -c ../cfg_FH420/per-sequence/${SeqName[$i]}.cfg --BitstreamFile=${SeqName[$i]}_qp${qplist[$qpind]}.bin --ReconFile=${SeqName[$i]}_qp${qplist[$qpind]}_rec.yuv $para --QP=${qplist[$qpind]} --TransformSkip=0 >./${SeqName[$i]}_qp${qplist[$qpind]}_enc_noTS.log &
+            cd $basedir/bin_HEVC_src
+            ./TAppEncoderStatic -c ../cfg_FH420/encoder_intra_main_MOL.cfg -c ../cfg_FH420/per-sequence/${SeqName[$i]}.cfg --BitstreamFile=${SeqName[$i]}_qp${qplist[$qpind]}.bin --ReconFile=${SeqName[$i]}_qp${qplist[$qpind]}_rec.yuv $para --QP=${qplist[$qpind]} >./${SeqName[$i]}_qp${qplist[$qpind]}_enc_TS.log
         }
     done
 done
